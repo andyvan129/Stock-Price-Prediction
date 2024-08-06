f3ef1e1abb77eb8fc55346d0730cbaf59b29b8e4
Andy Fan <andyvan129@gmail.com>
2024-08-06 15:31:42 -0500

    load packages

--- prediction.R
+++ prediction.R
@@ -1,2 +1,8 @@
 # building a stock price prediction model using historic SPY data
-# test
+
+# install required packages
+packages <- c('dplyr', 'caret')
+for (pack in packages) {
+  if (!require(pack, character.only = TRUE)) install.packages(pack)
+  library(pack, character.only = TRUE)
+}
