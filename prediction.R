e493969bbbdc948dc44b7ed2c25a2442659a2db7
Andy Fan <andyvan129@gmail.com>
2024-08-19 13:55:36 +0000

    Adding variables

--- prediction.R
+++ prediction.R
@@ -10,21 +10,24 @@ for (pack in packages) {
 rm(pack, packages)
 
 # loading stock pricing data
-SPY <- getSymbols(Symbols = 'SPY', auto.assign = FALSE) %>%
+stock <- getSymbols(Symbols = 'SPY', auto.assign = FALSE) %>%
   `colnames<-`(c('Open', 'High', 'Low', 'Close', 'Volume', 'Adjusted_Close'))
 
 # visualize price changes
-chart_Series(SPY)
+chart_Series(stock)
 
 # Compute price changes and create prediction target (y)
-SPY <- SPY %>%
+stock <- stock %>%
   data.frame() %>%
   mutate(change = Close - Open, change_pct = change * 100 / Open) %>%
-  mutate(direction = ifelse(change_pct > 0, 1, -1), y = lead(direction)) %>%
+  mutate(direction = ifelse(change_pct > 0, 1, 0), y = lead(direction)) %>%
   na.omit() %>%
   mutate(direction = NULL) # remove the known "direction" column.
 
+# Additional predictors can be added at this step
 
-
-
-
+# Filter out predictor columns
+# This also converts a timeseries data to individually unrelated data
+predictor_list <- c('change_pct', 'Volume')
+x <- stock %>%
+  select(any_of(predictor_list))
