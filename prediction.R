f117ddbe7ac89e7885b176e1e592c8b59d722d6e
Andy Fan <andyvan129@gmail.com>
2024-08-19 05:20:54 +0000

    Relative change

--- prediction.R
+++ prediction.R
@@ -10,10 +10,21 @@ for (pack in packages) {
 rm(pack, packages)
 
 # loading stock pricing data
-SPY <- data.frame(getSymbols(Symbols = 'SPY', auto.assign = FALSE)) %>%
+SPY <- getSymbols(Symbols = 'SPY', auto.assign = FALSE) %>%
   `colnames<-`(c('Open', 'High', 'Low', 'Close', 'Volume', 'Adjusted_Close'))
 
+# visualize price changes
 chart_Series(SPY)
 
-SPY[order(desc(as_datetime(rownames(SPY)))), ] %>%
-  top_n(10)
+# Compute price changes and create prediction target (y)
+SPY <- SPY %>%
+  data.frame() %>%
+  mutate(change = Close - Open, change_pct = change * 100 / Open) %>%
+  mutate(direction = ifelse(change_pct > 0, 1, -1), y = lead(direction)) %>%
+  na.omit() %>%
+  mutate(direction = NULL) # remove the known "direction" column.
+
+
+
+
+
